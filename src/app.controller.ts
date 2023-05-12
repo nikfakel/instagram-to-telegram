9b808ba9e9d3cb8a6beda3ae8421e157e297a893
nikokoshev <nikokoshev@adv.ru>
2023-05-12 06:12:02 -0500

    Test deployed version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {FirebaseService} from "./services/firebase.service";
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-  ) {}
+    private readonly firebaseService: FirebaseService
+  ) {
+
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  getPost() {
+    return this.firebaseService.getInstagramPost();
   }
 }
